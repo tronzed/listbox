b1c901f72677976a0d6271cd9fbc0c33209e7fb3
tanuj <you@3example3.com>
2026-04-21 20:57:32 +0530

    d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,9 +79,68 @@ export const Home = () => {
                         </div>
                     </div>
                 </div>
-                <div className="bottom_submit_box">
-                    <button>Submit</button>
+
+                <div className="bottom_fix_footer">
+                    <div className="bottom_view_box">
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">See List</button>
+                    </div>
+                    <div className="bottom_submit_box">
+                        <button>Submit</button>
+                    </div>
                 </div>
+
+                <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabIndex={-1}
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                                    Your Shoping List
+                                </h1>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                />
+                            </div>
+                            <div className="modal-body">
+
+                                {
+
+                                    addedList?.map((item, key) => (
+                                        <>
+                                            <p>
+                                                {item}
+                                            </p>
+                                        </>
+                                    ))
+
+                                }
+
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Close
+                                </button>
+                                <button type="button" className="btn btn-primary">
+                                    Save changes
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </>
         </>
     );
